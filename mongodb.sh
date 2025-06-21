6ca460851bf6d8d6f1561bbaf8a3ff2ed196920a
monnish-reddy <peta.monnish@gmail.com>
2025-06-20 20:04:45 -0400

    latest

--- mongodb.sh
+++ mongodb.sh
@@ -39,6 +39,12 @@ VALIDATE $? "Enabling MongoDB"
 systemctl start mongod 
 VALIDATE $? "Starting MongoDB"
 
+sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf
+VALIDATE $? "Editing MongoDB conf file for remote connections"
+
+systemctl restart mongod &>>$LOG_FILE
+VALIDATE $? "Restarting MongoDB"
+
 
 
 
